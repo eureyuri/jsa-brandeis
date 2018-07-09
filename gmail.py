523f2608fee1c647517f82dfbad435fbfe04217f
eureyuri <eureynoguchi@gmail.com>
2018-07-09 23:04:34 +0900

    Possible solution for gmail authentication

--- gmail.py
+++ gmail.py
@@ -8,17 +8,56 @@ from email.utils import formatdate
 import traceback
 import apiclient
 import argparse
+import json
+from dotenv import load_dotenv
+from pathlib import Path
 
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.send'
-CLIENT_SECRET_FILE = 'client_secret.json'
+# CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'client_secret_empty.json'
 # CLIENT_SECRET_FILE = os.environ.get("CLIENT_SECRET_FILE")
 APPLICATION_NAME = 'JSA Brandeis'
 MAIL_FROM = "eureynoguchi@gmail.com"
 MAIL_TO = "eurey@brandeis.edu"
 
 
+def build_client_secret():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+    with open(CLIENT_SECRET_FILE, 'r+') as f:
+        data = json.load(f)
+        # data['web']['client_id'] = os.environ.get("CLIENT_ID")
+        # data['web']['client_secret'] = os.environ.get("CLIENT_SECRET")
+        data['web']['client_id'] = os.getenv("CLIENT_ID")
+        data['web']['client_secret'] = os.getenv("CLIENT_SECRET")
+        # Reset file position to 0
+        f.seek(0)
+        json.dump(data, f)
+
+
+def delete_client_secret():
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+    with open(CLIENT_SECRET_FILE, 'r+') as f:
+        data = json.load(f)
+        # data['web']['client_id'] = os.environ.get("CLIENT_ID")
+        # data['web']['client_secret'] = os.environ.get("CLIENT_SECRET")
+        data['web']['client_id'] = ''
+        data['web']['client_secret'] = ''
+        # Reset file position to 0
+        f.seek(0)
+        json.dump(data, f)
+        # Remove remaining part because unwanted stuff gets concatenated for some reason
+        # Debug later when we have time
+        f.truncate()
+
+
 def get_credentials():
+    build_client_secret()
+
     # Get absolute path of current directory
     script_dir = os.path.abspath(os.path.dirname(__file__))
     credential_dir = os.path.join(script_dir, ".credentials")
@@ -37,6 +76,7 @@ def get_credentials():
         flags = argparse.ArgumentParser(parents=[oauth2client.tools.argparser]).parse_args()
         credentials = oauth2client.tools.run_flow(flow, store, flags)
         print("Storing credentials to " + credential_path)
+
     return credentials
 
 
@@ -58,6 +98,8 @@ def send_message(subject, message):
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = apiclient.discovery.build("gmail", "v1", http=http)
+
+    delete_client_secret()
 
     try:
         result = service.users().messages().send(
