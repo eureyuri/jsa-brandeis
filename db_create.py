031c23fd9fa2a2bdbabd2a857351192ad2a43604
eureyuri <eureynoguchi@gmail.com>
2018-07-17 22:48:27 +0900

    Implemented password hashing

--- db_create.py
+++ db_create.py
@@ -2,15 +2,14 @@ from jsabrandeis import db
 from model import User
 import os
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash
 
-
-# export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 db.create_all()
 
-admin = User(email=os.environ.get('ADMIN_EMAIL'), password=os.environ.get('ADMIN_PASSWORD'))
+admin = User(email=os.environ.get('ADMIN_EMAIL'), password=generate_password_hash(os.environ.get('ADMIN_PASSWORD')))
 
 db.session.add(admin)
 db.session.commit()
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.hybrid import hybrid_method
 from jsabrandeis import db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
@@ -7,13 +8,12 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String, unique=True, nullable=False)
-    # TODO: Hash
     password = db.Column(db.String, nullable=False)
     authenticated = db.Column(db.Boolean, default=False)
 
     def __init__(self, email, password):
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
         self.authenticated = False
 
     def get_id(self):
@@ -28,7 +28,7 @@ class User(db.Model):
 
     @hybrid_method
     def is_correct_password(self, password):
-        return self.password == password
+        return check_password_hash(self.password, password)
 
     @property
     def is_authenticated(self):
