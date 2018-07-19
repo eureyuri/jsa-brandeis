260d81572aeeeeaf10317f642e3fbb45b78b8d22
eureyuri <eureynoguchi@gmail.com>
2018-07-19 22:50:28 +0900

    Fix password hashing

--- db_create.py
+++ db_create.py
@@ -2,16 +2,13 @@ from jsabrandeis import db
 from model import User
 import os
 from dotenv import load_dotenv
-from werkzeug.security import generate_password_hash
 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 db.create_all()
 
-admin = User(email=os.environ.get('ADMIN_EMAIL'), password=generate_password_hash(os.environ.get('ADMIN_PASSWORD')))
-print(os.environ.get('ADMIN_EMAIL'))
-print(os.environ.get('ADMIN_PASSWORD'))
+admin = User(email=os.environ.get('ADMIN_EMAIL'), password=os.environ.get('ADMIN_PASSWORD'))
 
 db.session.add(admin)
 db.session.commit()
